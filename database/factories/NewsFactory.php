79f74d5ab2ebcae511e3abaeb96dae2a45e89c86
rusdeman <rusdeman@gmail.com>
2021-05-25 17:12:36 +0300

    add News

---
+++ database/factories/NewsFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\News;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class NewsFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = News::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(3),
+            'body' => $this->faker->sentence(9),
+            'status' => $this->faker->word()
+        ];
+    }
+}
